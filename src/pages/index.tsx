5681b6b21786cd4e98f4f70cd698022cc1fae01c
Henrique Barros <henriquesbarroshs@gmail.com>
2021-11-10 23:34:36 -0300

    test getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
-export default function Home() {
+import { GetServerSideProps } from "next";
+import { useState } from "react"
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  posts: Post[];
+}
+
+export default function Home({posts}: HomeProps) {
   return (
-    <h1>Hello, world!</h1>
+    <>
+      <h1>Post</h1>
+      <ul>
+        {posts.map(({id, title}) => (
+          <li key={id}>Título: {title}</li>
+        ))}
+      </ul>
+    </>
   )
+}
+
+// Esse método é chamado antes do componente ser renderizado.
+// O node que atua na camada intermediária é quem executa esta função.
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
