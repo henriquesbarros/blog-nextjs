777ad1b5673a04d071836ab39ff93237eb4982b9
henriquesbarros19 <henrique.barros@compasso.com.br>
2021-11-14 21:00:26 -0300

    feat: consuming prismic api and installing prismic-dom library

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,42 +1,77 @@
 import { GetStaticProps } from "next"
 import Link from 'next/link';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import SEO from "../../components/SEO";
+
+import { getPrismicClient } from "../../services/prismic";
 
 import styles from './posts.module.scss'
 
 interface Post {
-  id: string;
+  slug: string;
   title: string;
+  excerpt: string;
+  updateAt: string
 }
-
 interface PostsProps {
   posts: Post[];
 }
 
-export default function Posts() {
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <SEO title="Posts" excludeTitleSufix/>
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <Link href='#'>
-            <a>
-              <time>14 de novembro de 2021</time>
-              <strong>Título</strong>
-              <p>Parágrafo</p>
-            </a>
-          </Link>
+          {posts.map(post => (
+            <Link href='#' key={post.slug}>
+              <a>
+                <time>{post.updateAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   )
 }
 
+// Executando em nível de back-end
 export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const {results} = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+    },
+  );
+
+  const posts = results.map(post => {
     return {
-      props: {},
-      revalidate: 60 * 60 * 12 // 12H
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        },
+      ),
+    }
+  })
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 60 * 60 * 12 // 12H
   }
 }
